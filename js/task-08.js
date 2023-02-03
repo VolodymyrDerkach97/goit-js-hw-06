f11e05d1a38c8cc5ea73bf624956d8e4aae8ea1d
VolodymyrDerkach97 <bobikderkac@gmail.com>
2023-02-03 19:40:34 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,8 +2,20 @@ const form = document.querySelector('.login-form');
 
 
 form.addEventListener('submit', onCheckFormValid)
-console.dir(form.label);
+
 function onCheckFormValid(event) {
     event.preventDefault();
-
+    const formData = new FormData(event.currentTarget);
+    
+    if (formData.get('password') === '' || formData.get('email') === '') {
+        alert('Всі поля повинні бути заповнені')
+    };
+    console.log(formData);
+    console.log(formData.set(values, name));
+    
+    formData.forEach((values, name) => {
+        const dataFormResult = {values, name}
+        return console.log(dataFormResult)
+    })
+    ;
 }
